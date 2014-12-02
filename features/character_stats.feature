6c642bffd52066afbed5b3cda61b742774795318
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-12-02 12:04:58 -0500

    Remove features directory
    The directory was full of half-completed tests. Thus, I am removing
    it in order to clean up the project.

--- features/character_stats.feature
+++
@@ -1,4 +0,0 @@
-Feature: Viewing character statistics
-
-Background:
-  Given I have created a new character
--- features/step_definitions/character_stats_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'characters/character'
-
-Given(/^I have created a new character$/) do
-  @character = Character.new( {name: "Bob"} )
-end
